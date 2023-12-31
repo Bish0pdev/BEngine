cdccf548a023292aa7e32145e1ec7cc3c71b2b5c
Bish0pdev <Ytthepienetwork@gmail.com>
2023-12-30 21:15:28 -0600

    BCore Changes

--- Game/src/Game1.cs
+++ Game/src/Game1.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using System.Collections.Generic;
+using BCore;
+using System;
 namespace BEngine
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Camera mainCam;
+        private List<Entity> activeEntitys = new List<Entity>();
 
         public Game1()
         {
@@ -19,7 +23,20 @@ namespace BEngine
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            mainCam = new Camera(new Vector2(-Window.ClientBounds.Width/2,-Window.ClientBounds.Height/2));
 
+            Texture2D circletex = SimpleTextures.CreateCircleTextureData(10, Color.White, GraphicsDevice);
+            Texture2D square = SimpleTextures.CreateSquareTexture(GraphicsDevice, 100, Color.AntiqueWhite);
+            Entity circle = new Entity();
+            circle.AddComponent(new SpriteRenderer(circletex));
+            circle.AddComponent(new PhysicsComponent());
+            activeEntitys.Add(circle);
+
+            Entity Floor = new Entity();
+            Floor.AddComponent(new SpriteRenderer(square));
+            Floor.AddComponent(new BoxCollider());
+            Floor.MoveTo(new Vector2(0, 100));
+            activeEntitys.Add(Floor);
             base.Initialize();
         }
 
@@ -35,16 +52,41 @@ namespace BEngine
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Move the camera based on keyboard input
+            KeyboardState keyboardState = Keyboard.GetState();
 
+            float cameraSpeed = 5f; // Adjust the speed as needed
+            Vector2 cameraMovement = Vector2.Zero;
+
+            if (keyboardState.IsKeyDown(Keys.W))
+                cameraMovement.Y -= cameraSpeed;
+            if (keyboardState.IsKeyDown(Keys.A))
+                cameraMovement.X -= cameraSpeed;
+            if (keyboardState.IsKeyDown(Keys.S))
+                cameraMovement.Y += cameraSpeed;
+            if (keyboardState.IsKeyDown(Keys.D))
+                cameraMovement.X += cameraSpeed;
+
+            mainCam.Position += cameraMovement;
+
+            for (int i = 0; i < activeEntitys.Count; i++)
+            {
+                activeEntitys[i].Update(gameTime);
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            
+            _spriteBatch.Begin(transformMatrix: mainCam.TransformMatrix);
 
-            // TODO: Add your drawing code here
+            for (int i = 0; i < activeEntitys.Count; i++)
+            {
+                activeEntitys[i].Draw(_spriteBatch);
+            }
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
